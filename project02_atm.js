e0bd0dd86fce90e6bf8b25b83d412e274b8864e1
ameenilyas <ameenilyas78@gmail.com>
2022-10-17 20:45:53 +0500

    initialize

---
+++ project02_atm.js
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+const atm_pin = Math.floor(Math.random() * 9999) + "";
+const uid = Math.floor(Math.random() * 1000) + "";
+console.log({ atm_pin, uid });
+async function getAtmPin(message_type) {
+    const answers = await inquirer.prompt({
+        name: "number",
+        type: "input",
+        message: `Enter your ${message_type}.`,
+        default() {
+            return 10;
+        },
+    });
+    return answers.number;
+}
+const input_atm_pin = await getAtmPin("Atm Pin");
+const input_uid = await getAtmPin("User Id");
+if (atm_pin === input_atm_pin && uid === input_uid) {
+    console.log("Greetings Mr. X, You may proceed.");
+}
+else {
+    console.log("Invalid Credentials.");
+}
